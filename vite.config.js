b0499f9c79561528f3967920e4dd0dd65f3a61e3
Hector solano <119557633+monocuco01@users.noreply.github.com>
2025-11-29 02:13:37 -0500

    zumbaa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",   // 👈 IMPORTANTE PARA QUE VERCEL SIRVA BIEN LAS RUTAS
   plugins: [react()],
 })
